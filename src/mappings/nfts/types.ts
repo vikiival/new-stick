20cdbb4a79c8a9da4f5ab2bf967c16c9b59ae5f4
Viki Val <viktorko99@gmail.com>
2023-05-22 16:29:22 +0200

    :zap: logical swaps

--- src/mappings/nfts/types.ts
+++ src/mappings/nfts/types.ts
@@ -1,24 +1,37 @@
+
 import { BaseTokenEvent } from '../uniques/types'
+import { str } from '../utils/helper'
 
 export type WithAmount = {
   amount: bigint;
+};
+
+export type WithOwner = {
+  owner: string;
 };
 
 export type PayRoyaltyEvent = BaseTokenEvent & WithAmount & {
   recipient: string;
 }
 
+export type Target = BaseTokenEvent & {
+  sn?: string;
+}
+
 export type CreateSwapEvent = BaseTokenEvent & WithAmount & {
   expiresAt: bigint;
   surcharge?: 'Send' | 'Receive';
-  swapCollectionId: string;
-  swapSn?: string;
+  target: Target;
 }
 
 export type CancelSwapEvent = CreateSwapEvent
 
-export type ClaimSwapEvent = CreateSwapEvent & {
-  swapSn: string;
-  owner: string;
-  swapOwner: string;
+export type ClaimSwapEvent = CreateSwapEvent & WithOwner & {
+  target: Target & WithOwner;
 }
+
+
+export const createTarget = (collection: number, tokenId: number | undefined): Target => ({
+  collectionId: str(collection),
+  sn: tokenId ? str(tokenId) : '',
+})
