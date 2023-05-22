323a2aeded851c4d3e614f742d9b49767491504e
Viki Val <viktorko99@gmail.com>
2023-05-22 16:34:01 +0200

    :construction: WIP createSwap

--- src/mappings/nfts/createSwap.ts
+++ src/mappings/nfts/createSwap.ts
@@ -1,3 +1,23 @@
-import { Context } from '../utils/types'
+import { getOrFail as get } from '@kodadot1/metasquid/entity'
+import {
+  NFTEntity as NE
+} from '../../model'
+import { createEvent } from '../shared/event'
+import { unwrap } from '../utils/extract'
+import { debug, pending, warn } from '../utils/logger'
+import { Context, createTokenId } from '../utils/types'
+import { getCreateSwapEvent } from './getters'
 
-export async function handleSwapCreate(context: Context): Promise<void> {}
+const OPERATION: any = 'CREATE_SWAP' // Action.PAY_ROYALTY
+
+export async function handleSwapCreate(context: Context): Promise<void> {
+  pending(OPERATION, `${context.block.height}`);
+  const event = unwrap(context, getCreateSwapEvent);
+  debug(OPERATION, event, true);
+
+  const id = createTokenId(event.collectionId, event.sn);
+  const entity = await get(context.store, NE, id);
+
+  
+
+}
