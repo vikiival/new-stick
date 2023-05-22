32b43aea2ef9c1823f4ce3845747ea1262a66673
Viki Val <viktorko99@gmail.com>
2023-05-22 16:29:43 +0200

    :broom: format

--- src/mappings/nfts/getters.ts
+++ src/mappings/nfts/getters.ts
@@ -1,85 +1,180 @@
-import { NftsSwapCancelledEvent, NftsSwapClaimedEvent, NftsSwapCreatedEvent, NftsTipSentEvent } from '../../types/events'
-import { addressOf, str } from '../utils/helper'
+import {
+  NftsSwapCancelledEvent,
+  NftsSwapClaimedEvent,
+  NftsSwapCreatedEvent,
+  NftsTipSentEvent,
+} from '../../types/events'
+import { addressOf } from '../utils/helper'
 import { Context } from '../utils/types'
-import { CancelSwapEvent, ClaimSwapEvent, CreateSwapEvent, PayRoyaltyEvent } from './types'
+import {
+  CancelSwapEvent,
+  ClaimSwapEvent,
+  CreateSwapEvent,
+  PayRoyaltyEvent,
+  createTarget,
+} from './types'
 
 export function getPayRoyaltyEvent(ctx: Context): PayRoyaltyEvent {
-  const event = new NftsTipSentEvent(ctx);
+  const event = new NftsTipSentEvent(ctx)
   if (event.isV9370) {
     const {
-      collection: classId, item: instanceId, receiver: recipient, amount,
+      collection: classId,
+      item: instanceId,
+      receiver: recipient,
+      amount,
     } = event.asV9370
     return {
-      collectionId: classId.toString(), sn: instanceId.toString(), recipient: addressOf(recipient), amount,
-    };
+      collectionId: classId.toString(),
+      sn: instanceId.toString(),
+      recipient: addressOf(recipient),
+      amount,
+    }
   }
 
-    ctx.log.warn('USING UNSAFE GETTER! PLS UPDATE TYPES!')
-    const {
-      collection: classId, item: instanceId, receiver: recipient, amount,
-    } = ctx._chain.decodeEvent(ctx.event)
+  ctx.log.warn('USING UNSAFE GETTER! PLS UPDATE TYPES!')
+  const {
+    collection: classId,
+    item: instanceId,
+    receiver: recipient,
+    amount,
+  } = ctx._chain.decodeEvent(ctx.event)
 
-    return {
-      collectionId: classId.toString(), sn: instanceId.toString(), recipient: addressOf(recipient), amount,
-    };
+  return {
+    collectionId: classId.toString(),
+    sn: instanceId.toString(),
+    recipient: addressOf(recipient),
+    amount,
+  }
 }
 
 export function getCreateSwapEvent(ctx: Context): CreateSwapEvent {
-  const event = new NftsSwapCreatedEvent(ctx);
+  const event = new NftsSwapCreatedEvent(ctx)
 
   if (event.isV9370) {
     const {
-      offeredCollection: classId, offeredItem: instanceId, desiredCollection, desiredItem, price, deadline
-    } = event.asV9370;
+      offeredCollection: classId,
+      offeredItem: instanceId,
+      desiredCollection,
+      desiredItem,
+      price,
+      deadline,
+    } = event.asV9370
     return {
-      collectionId: classId.toString(), sn: instanceId.toString(), amount: price?.amount || 0n, surcharge: price?.direction?.__kind, expiresAt: BigInt(deadline), swapCollectionId: str(desiredCollection), swapSn: desiredItem?.toString(),
-    };
+      collectionId: classId.toString(),
+      sn: instanceId.toString(),
+      amount: price?.amount || 0n,
+      surcharge: price?.direction?.__kind,
+      expiresAt: BigInt(deadline),
+      target: createTarget(desiredCollection, desiredItem),
+    }
   }
 
   const {
-    offeredCollection: classId, offeredItem: instanceId, desiredCollection, desiredItem, price, deadline
-  } = ctx._chain.decodeEvent(ctx.event);
+    offeredCollection: classId,
+    offeredItem: instanceId,
+    desiredCollection,
+    desiredItem,
+    price,
+    deadline,
+  } = ctx._chain.decodeEvent(ctx.event)
   return {
-    collectionId: classId.toString(), sn: instanceId.toString(), amount: price?.amount || 0n, surcharge: price?.direction?.__kind, expiresAt: BigInt(deadline), swapCollectionId: str(desiredCollection), swapSn: desiredItem?.toString(),
-  };
+    collectionId: classId.toString(),
+    sn: instanceId.toString(),
+    amount: price?.amount || 0n,
+    surcharge: price?.direction?.__kind,
+    expiresAt: BigInt(deadline),
+    target: createTarget(desiredCollection, desiredItem),
+  }
 }
 
 export function getCancelSwapEvent(ctx: Context): CancelSwapEvent {
-  const event = new NftsSwapCancelledEvent(ctx);
+  const event = new NftsSwapCancelledEvent(ctx)
 
   if (event.isV9370) {
     const {
-      offeredCollection: classId, offeredItem: instanceId, desiredCollection, desiredItem, price, deadline
-    } = event.asV9370;
+      offeredCollection: classId,
+      offeredItem: instanceId,
+      desiredCollection,
+      desiredItem,
+      price,
+      deadline,
+    } = event.asV9370
     return {
-      collectionId: classId.toString(), sn: instanceId.toString(), amount: price?.amount || 0n, surcharge: price?.direction?.__kind, expiresAt: BigInt(deadline), swapCollectionId: str(desiredCollection), swapSn: desiredItem?.toString(),
-    };
+      collectionId: classId.toString(),
+      sn: instanceId.toString(),
+      amount: price?.amount || 0n,
+      surcharge: price?.direction?.__kind,
+      expiresAt: BigInt(deadline),
+      target: createTarget(desiredCollection, desiredItem),
+    }
   }
 
   const {
-    offeredCollection: classId, offeredItem: instanceId, desiredCollection, desiredItem, price, deadline
-  } = ctx._chain.decodeEvent(ctx.event);
+    offeredCollection: classId,
+    offeredItem: instanceId,
+    desiredCollection,
+    desiredItem,
+    price,
+    deadline,
+  } = ctx._chain.decodeEvent(ctx.event)
   return {
-    collectionId: classId.toString(), sn: instanceId.toString(), amount: price?.amount || 0n, surcharge: price?.direction?.__kind, expiresAt: BigInt(deadline), swapCollectionId: str(desiredCollection), swapSn: desiredItem?.toString(),
-  };
+    collectionId: classId.toString(),
+    sn: instanceId.toString(),
+    amount: price?.amount || 0n,
+    surcharge: price?.direction?.__kind,
+    expiresAt: BigInt(deadline),
+    target: createTarget(desiredCollection, desiredItem),
+  }
 }
 
 export function getClaimSwapEvent(ctx: Context): ClaimSwapEvent {
-  const event = new NftsSwapClaimedEvent(ctx);
+  const event = new NftsSwapClaimedEvent(ctx)
 
   if (event.isV9370) {
     const {
-      sentCollection: classId, sentItem: instanceId, sentItemOwner, receivedCollection, receivedItem, receivedItemOwner, price, deadline
-    } = event.asV9370;
+      sentCollection: classId,
+      sentItem: instanceId,
+      sentItemOwner,
+      receivedCollection,
+      receivedItem,
+      receivedItemOwner,
+      price,
+      deadline,
+    } = event.asV9370
     return {
-      collectionId: classId.toString(), sn: instanceId.toString(), owner: addressOf(sentItemOwner), amount: price?.amount || 0n, surcharge: price?.direction?.__kind, expiresAt: BigInt(deadline), swapCollectionId: str(receivedCollection), swapSn: str(receivedItem), swapOwner: addressOf(receivedItemOwner),
-    };
+      collectionId: classId.toString(),
+      sn: instanceId.toString(),
+      owner: addressOf(sentItemOwner),
+      amount: price?.amount || 0n,
+      surcharge: price?.direction?.__kind,
+      expiresAt: BigInt(deadline),
+      target: {
+        ...createTarget(receivedCollection, receivedItem),
+        owner: addressOf(receivedItemOwner),
+      }
+    }
   }
 
   const {
-    sentCollection: classId, sentItem: instanceId, sentItemOwner, receivedCollection, receivedItem, receivedItemOwner, price, deadline
-  } = ctx._chain.decodeEvent(ctx.event);
+    sentCollection: classId,
+    sentItem: instanceId,
+    sentItemOwner,
+    receivedCollection,
+    receivedItem,
+    receivedItemOwner,
+    price,
+    deadline,
+  } = ctx._chain.decodeEvent(ctx.event)
   return {
-    collectionId: classId.toString(), sn: instanceId.toString(), owner: addressOf(sentItemOwner), amount: price?.amount || 0n, surcharge: price?.direction?.__kind, expiresAt: BigInt(deadline), swapCollectionId: str(receivedCollection), swapSn: str(receivedItem), swapOwner: addressOf(receivedItemOwner),
-  };
+    collectionId: classId.toString(),
+    sn: instanceId.toString(),
+    owner: addressOf(sentItemOwner),
+    amount: price?.amount || 0n,
+    surcharge: price?.direction?.__kind,
+    expiresAt: BigInt(deadline),
+    target: {
+      ...createTarget(receivedCollection, receivedItem),
+      owner: addressOf(receivedItemOwner),
+    }
+  }
 }
